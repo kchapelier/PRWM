4b70c559d59cac876f7a8144fa99695ee03d5bf7
kchapelier <kevin.chapelier@gmail.com>
2017-06-03 17:34:54 +0200

    prwm: add models, generate prwm directly from obj files

---
+++ models/generate-prwm-models-from-obj.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+./../implementations/packed-raw-webgl/obj2prwm/index.js -i ./ascii-obj/unit-cube.obj -o ./prwm/cube-LE.prwm --positions --normals --indexed
+./../implementations/packed-raw-webgl/obj2prwm/index.js -i ./ascii-obj/unit-cube.obj -o ./prwm/cube-BE.prwm --positions --normals --indexed --be
+./../implementations/packed-raw-webgl/obj2prwm/index.js -i ./ascii-obj/smooth-suzanne.obj -o ./prwm/smooth-suzanne-LE.prwm --positions --normals --indexed
+./../implementations/packed-raw-webgl/obj2prwm/index.js -i ./ascii-obj/smooth-suzanne.obj -o ./prwm/smooth-suzanne-BE.prwm --positions --normals --indexed --be
+./../implementations/packed-raw-webgl/obj2prwm/index.js -i ./ascii-obj/faceted-nefertiti.obj -o ./prwm/faceted-nefertiti-LE.prwm --positions --normals --indexed
+./../implementations/packed-raw-webgl/obj2prwm/index.js -i ./ascii-obj/faceted-nefertiti.obj -o ./prwm/faceted-nefertiti-BE.prwm --positions --normals --indexed --be
+./../implementations/packed-raw-webgl/obj2prwm/index.js -i ./ascii-obj/smooth-nefertiti.obj -o ./prwm/smooth-nefertiti-LE.prwm --positions --normals --indexed
+./../implementations/packed-raw-webgl/obj2prwm/index.js -i ./ascii-obj/smooth-nefertiti.obj -o ./prwm/smooth-nefertiti-BE.prwm --positions --normals --indexed --be
+./../implementations/packed-raw-webgl/obj2prwm/index.js -i ./ascii-obj/vr_controller_vive_1_5.tri.obj -o ./prwm/vive-controller-LE.prwm --positions --normals --indexed
+./../implementations/packed-raw-webgl/obj2prwm/index.js -i ./ascii-obj/vr_controller_vive_1_5.tri.obj -o ./prwm/vive-controller-BE.prwm --positions --normals --indexed --be
+./../implementations/packed-raw-webgl/obj2prwm/index.js -i ./ascii-obj/cerberus.obj -o ./prwm/cerberus-LE.prwm --positions --normals --indexed
+./../implementations/packed-raw-webgl/obj2prwm/index.js -i ./ascii-obj/cerberus.obj -o ./prwm/cerberus-BE.prwm --positions --normals --indexed --be
