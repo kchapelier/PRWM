ed793baa549ede10d07f7e2b0e608fcfd52627b8
kchapelier <kevin.chapelier@gmail.com>
2017-06-10 14:27:41 +0200

    models: make the faceted nerfertiti non-indexed geometries

--- models/generate-prwm-models-from-obj.sh
+++ models/generate-prwm-models-from-obj.sh
@@ -4,8 +4,8 @@
 ./../implementations/obj2prwm/cli.js -i ./ascii-obj/unit-cube.obj -o ./prwm/cube-BE.prwm --positions --normals --indexed -q --be
 ./../implementations/obj2prwm/cli.js -i ./ascii-obj/smooth-suzanne.obj -o ./prwm/smooth-suzanne-LE.prwm --positions --normals --indexed -q
 ./../implementations/obj2prwm/cli.js -i ./ascii-obj/smooth-suzanne.obj -o ./prwm/smooth-suzanne-BE.prwm --positions --normals --indexed -q --be
-./../implementations/obj2prwm/cli.js -i ./ascii-obj/faceted-nefertiti.obj -o ./prwm/faceted-nefertiti-LE.prwm --positions --normals --indexed -q
-./../implementations/obj2prwm/cli.js -i ./ascii-obj/faceted-nefertiti.obj -o ./prwm/faceted-nefertiti-BE.prwm --positions --normals --indexed -q --be
+./../implementations/obj2prwm/cli.js -i ./ascii-obj/faceted-nefertiti.obj -o ./prwm/faceted-nefertiti-LE.prwm --positions --normals -q
+./../implementations/obj2prwm/cli.js -i ./ascii-obj/faceted-nefertiti.obj -o ./prwm/faceted-nefertiti-BE.prwm --positions --normals -q --be
 ./../implementations/obj2prwm/cli.js -i ./ascii-obj/smooth-nefertiti.obj -o ./prwm/smooth-nefertiti-LE.prwm --positions --normals --indexed -q
 ./../implementations/obj2prwm/cli.js -i ./ascii-obj/smooth-nefertiti.obj -o ./prwm/smooth-nefertiti-BE.prwm --positions --normals --indexed -q --be
 ./../implementations/obj2prwm/cli.js -i ./ascii-obj/vr_controller_vive_1_5.tri.obj -o ./prwm/vive-controller-LE.prwm --positions --normals --indexed -q
